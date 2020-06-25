3a9f2f33ac8ff09b5026192201c36da0bd5bcd2f
unknown <rodd1996@gmail.com>
2020-06-24 23:43:16 -0300

    Começo do refatoramento feito

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const nunjucks = require("nunjucks")
+
+const server = express()
+
+server.listen(3000, function(){
+    console.log("server is running")
+}) 
+//const courses = require("./data")
+
+server.use(express.static("public"))
+
+server.set("view engine", "html")
+
+server.get("/", function(req, res){
+    return res.render("home")
+})
+
+server.get("/sobre", function(req, res){
+    return res.render("about")
+})
+
+server.get("/receitas", function(req, res){
+    return res.render("recepies")
+})
+
+nunjucks.configure("views", {
+    express:server,
+    autoescape: false
+})
